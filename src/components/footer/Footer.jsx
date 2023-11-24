e67187477533840d288854775d323ac2fd9f639a
Marina Matulle <marinamatulle.mm@gmail.com>
2023-11-24 16:36:19 -0300

    adds style and implements footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,20 @@
-import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer>
-            FOOTER
-        </footer>
-    );
-}
+  return (
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a href="">Sobre mim</a>
+        </li>
+        <li>
+          <a href="">Projetos</a>
+        </li>
+      </ul>
+
+      <p>© 2023 Marina Matulle. All rights reserved.</p>
+    </footer>
+  );
+};
 
 export default Footer;
